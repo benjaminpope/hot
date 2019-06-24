e7277adea5557efd722377c46721ad30fd9ebdab
Benjamin Pope <benjamin.pope@nyu.edu>
2019-06-24 16:23:53 -0400

    kic

--- notebooks/hot_script.py
+++ notebooks/hot_script.py
@@ -22,4 +22,8 @@ if __name__ == '__main__':
     ap = ArgumentParser(description='hot: look for planets around hot stars with iterative sine fitting')
     ap.add_argument('-kic', default=8197761,type=int,help='Target KIC ID')
 
+    args = ap.parse_args()
+
+    kic = args.kic
+
     do_all(kic)
