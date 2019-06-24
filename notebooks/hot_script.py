4c4156ddbb88edc408f28ba07731825102ab1291
Benjamin Pope <benjamin.pope@nyu.edu>
2019-06-24 16:21:02 -0400

    argparse

--- notebooks/hot_script.py
+++ notebooks/hot_script.py
@@ -1,5 +1,22 @@
 from hot_utils import *
 
+from argparse import ArgumentParser
+
+mpl.style.use('seaborn-colorblind')
+
+#To make sure we have always the same matplotlib settings
+#(the ones in comments are the ipython notebook settings)
+
+mpl.rcParams['figure.figsize']=(8.0,6.0)    #(6.0,4.0)
+mpl.rcParams['font.size']=18               #10 
+mpl.rcParams['savefig.dpi']= 200             #72 
+mpl.rcParams['axes.labelsize'] = 16
+mpl.rcParams['axes.labelsize'] = 16
+mpl.rcParams['xtick.labelsize'] = 12
+mpl.rcParams['ytick.labelsize'] = 12
+mpl.rcParams["font.family"] = "Times New Roman"
+
+
 if __name__ == '__main__':
     ap = ArgumentParser(description='hot: look for planets around hot stars with iterative sine fitting')
     ap.add_argument('-kic', default=8197761,type=int,help='Target KIC ID')
